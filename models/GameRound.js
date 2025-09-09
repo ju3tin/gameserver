5ed30e6eb809270d8d5f6ab50b33b18020c52028
Justin <ju3tin@hotmail.co.uk>
2025-09-09 15:17:16 +0100

    Update GameRound.js

--- models/GameRound.js
+++ models/GameRound.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
-
 const BetSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  walletAddress: { type: String, required: true },
   amount: Number,
   currency: String,
-  cashedOut: { type: Boolean, default: false },
+  cashedOut: { type: Boolean, default: false }
 });
 
 const GameRoundSchema = new mongoose.Schema({
   startTime: Date,
   crashMultiplier: Number,
-  bets: [BetSchema],
+  bets: [BetSchema]
 });
 
 module.exports = mongoose.model('GameRound', GameRoundSchema);
