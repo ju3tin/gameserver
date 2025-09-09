ef4e24f5383d659c61d72127932ad5fd9bcf1c75
Justin <ju3tin@hotmail.co.uk>
2025-09-09 14:09:56 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,4 +8,15 @@ router.get('/', async (req, res) => {
   res.json(users);
 });
 
+
+router.get('/all', async (req, res) => {
+  try {
+    const users = await User.find().select('-__v'); // Exclude __v field
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Failed to fetch users.' });
+  }
+});
+
 module.exports = router;
