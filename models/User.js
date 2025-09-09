1f9df82c519a5b19d15e78cba98013d3f152acda
Justin <ju3tin@hotmail.co.uk>
2025-09-09 14:05:04 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  walletAddress: { type: String, required: true, unique: true },
   balances: {
     SOL: { type: Number, default: 1000 },
     CHIPPY: { type: Number, default: 1000 },
