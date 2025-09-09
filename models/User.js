2dd47992b5b4893344bc5479eef7b973d78321f4
Justin <ju3tin@hotmail.co.uk>
2025-09-09 13:40:45 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  balances: {
+    SOL: { type: Number, default: 1000 },
+    CHIPPY: { type: Number, default: 1000 },
+    DEMO: { type: Number, default: 1000 }
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
